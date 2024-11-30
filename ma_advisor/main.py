a36fd87d792910460a32e5d8b69e914e76e1a1ff
Moncif <moncif.el-mouden@etu.u-pec.fr>
2024-11-30 16:29:22 +0100

    streamlit gui hello world

--- ma_advisor/main.py
+++ ma_advisor/main.py
@@ -1,9 +1,5 @@
-from agents.basic_agent import graph, query_basic_agent
+import streamlit as st
 
-print(graph.get_graph())
 
-question = "how much is the volume of the world"
-
-print(query_basic_agent(query=question))
-
-    
+with st.chat_message("user"):
+    st.write("hello world")
