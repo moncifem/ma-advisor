84cb9c2e246cd735d02ef987df81af7fb7fa4a4e
Moncif <moncif.el-mouden@etu.u-pec.fr>
2024-11-30 16:18:50 +0100

    :test_tube: Added test package and delete of unused files

---
+++ tests/README.md
@@ -0,0 +1,11 @@
+# M&A advisor
+
+## Test package
+
+A test package for the functionalities of the agents and utils
+
+### Usage/Examples
+
+```bash
+python -m tests.test_basic_agent
+```
--- utils/model.py
+++
@@ -1,7 +0,0 @@
-from langchain_openai import ChatOpenAI
-
-def gpt_35_turbo():
-    return ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-
-def gpt_4o():
-    return ChatOpenAI(model="gpt-4o", temperature=0)
