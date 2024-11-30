28cf633d148f05dc335449d278afdad8ad8670b4
Moncif <moncif.el-mouden@etu.u-pec.fr>
2024-11-30 16:17:26 +0100

    :package: Refactor of the project

---
+++ ma_advisor/main.py
@@ -0,0 +1,9 @@
+from agents.basic_agent import graph, query_basic_agent
+
+print(graph.get_graph())
+
+question = "how much is the volume of the world"
+
+print(query_basic_agent(query=question))
+
+    
